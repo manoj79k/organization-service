6639e4f3d04310d8b0a4283e331301b816c54393
Manoj Kumar <manoj.79k@gmail.com>
2018-12-05 20:29:55 -0500

    added circuit breaker

--- src/main/java/com/mk/organization/Application.java
+++ src/main/java/com/mk/organization/Application.java
@@ -3,6 +3,7 @@ package com.mk.organization;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 //@EnableFeignClients
+@EnableCircuitBreaker
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/mk/organization/services/OrganizationService.java
+++ src/main/java/com/mk/organization/services/OrganizationService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 @Service
@@ -15,7 +16,10 @@ public class OrganizationService {
     private OrganizationRepository orgRepository;
 
     public Organization getOrg(String organizationId) {
-        return orgRepository.findById(organizationId);
+    	System.out.println("OrganizationService server call start");
+    	randomlyRunLong();
+    	System.out.println("OrganizationService server call end");
+    	return orgRepository.findById(organizationId);
     }
     
     public List<Organization> getOrgs() {
@@ -37,4 +41,20 @@ public class OrganizationService {
     public void deleteOrg(Organization org){
         orgRepository.delete( org.getId());
     }
+    
+    private void randomlyRunLong(){
+        Random rand = new Random();
+
+        int randomNum = rand.nextInt((3 - 1) + 1) + 1;
+
+        if (randomNum==3) sleep();
+      }
+
+      private void sleep(){
+          try {
+              Thread.sleep(12000);
+          } catch (InterruptedException e) {
+              e.printStackTrace();
+          }
+      } 
 }
